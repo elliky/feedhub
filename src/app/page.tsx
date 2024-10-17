30ed98a9e1ab7599630ff96fa91dea41b97802c6
elliky <stefan.kueenzi@gmail.com>
2024-10-17 16:11:24 +0200

    include firebase example and routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
 
-import AddItem from '../components/AddItem';
+export default function Page() {
 
-export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          <h1>Welcome to My Next.js App</h1>
-          <AddItem />
-        </div>
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          <li>
+            <Link href="/about">About Us</Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">Blog Post</Link>
+          </li>
+        </ul>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
